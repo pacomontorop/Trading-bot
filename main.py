84ee4399e221582f19abd3ffe9ac37097fed0f48
pacomontorop <pacomontorop@gmail.com>
2025-05-02 14:12:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 if __name__ == "__main__":
     print("🟢 Lanzando schedulers...")
     start_schedulers()
-
-    # 🔁 Mantener vivo el proceso aunque todos los hilos sean daemon
     while True:
-        time.sleep(3600)
+        print("⏳ Main thread activo, esperando...")
+        time.sleep(600)
