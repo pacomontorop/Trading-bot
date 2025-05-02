469bdc539c7ea447e24bc2f30e0cd655f500cc4e
pacomontorop <pacomontorop@gmail.com>
2025-05-02 11:18:33 +0200

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,9 @@
+from datetime import datetime
+
+def log_event(message):
+    timestamp = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    log_line = f"[{timestamp}] {message}"
+    print(log_line)
+    with open("trading_log.txt", "a") as log_file:
+        log_file.write(log_line + "\n")
 # log_event con logging
