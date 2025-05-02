1a15da442b0e0d935c0b40f562a31ad0b6cf5449
pacomontorop <pacomontorop@gmail.com>
2025-05-02 13:53:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from core.scheduler import start_schedulers
-import time
-
-def main():
-    print("Iniciando schedulers...")
-    start_schedulers()  # lanza los hilos, procesos o jobs programados
-    while True:
-        print("Esperando...")
-        time.sleep(60)
 
 if __name__ == "__main__":
-    main()
+    print("🟢 Lanzando todos los schedulers...")
+    start_schedulers()
