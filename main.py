9d9caf3213d3fd502f339fc93cbcbb2c042742f2
pacomontorop <pacomontorop@gmail.com>
2025-05-02 13:57:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from core.scheduler import start_schedulers
+import traceback
 
 if __name__ == "__main__":
-    print("🟢 Lanzando todos los schedulers...")
-    start_schedulers()
+    print("🟢 Lanzando schedulers...")
+
+    try:
+        start_schedulers()
+    except Exception as e:
+        print("❌ Error en el proceso principal:")
+        traceback.print_exc()
