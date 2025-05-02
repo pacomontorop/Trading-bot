f9bc04f655e36e503a6b54777d6b60c07421be58
pacomontorop <pacomontorop@gmail.com>
2025-05-02 14:03:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from core.scheduler import start_schedulers
-import traceback
+import time
 
 if __name__ == "__main__":
     print("🟢 Lanzando schedulers...")
+    start_schedulers()
 
-    try:
-        start_schedulers()
-    except Exception as e:
-        print("❌ Error en el proceso principal:")
-        traceback.print_exc()
+    # 🔁 Mantener vivo el proceso aunque todos los hilos sean daemon
+    while True:
+        time.sleep(3600)
