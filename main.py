8c3aadf5d30f4d590797409bda7989bda94da900
pacomontorop <pacomontorop@gmail.com>
2025-05-02 13:47:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from core.scheduler import start_schedulers
 
+import time
+
+def main():
+    while True:
+        print("Ejecutando tareas...")
+        # Aquí va tu lógica de trading, lectura de señales, etc.
+        time.sleep(60)  # espera 1 minuto antes de repetir
+
 if __name__ == "__main__":
-    start_schedulers()
+    main()
