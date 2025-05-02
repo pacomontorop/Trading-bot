e158e4995454a94d6b2cf382f5aecbf571b37ebf
pacomontorop <pacomontorop@gmail.com>
2025-05-02 14:17:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from core.scheduler import start_schedulers
-import time
 
 if __name__ == "__main__":
     print("🟢 Lanzando schedulers...")
     start_schedulers()
-    while True:
-        print("⏳ Main thread activo, esperando...")
-        time.sleep(600)
