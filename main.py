2514a69b75e52459a5e2ee6cc09acc4bcc63feeb
pacomontorop <pacomontorop@gmail.com>
2025-05-02 15:05:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from core.scheduler import start_schedulers
+import time
 
 if __name__ == "__main__":
-    print("🟢 Lanzando schedulers...")
+    print("🟢 Lanzando schedulers...", flush=True)
     start_schedulers()
+
+    # 🔁 Mantener vivo el proceso aunque todos los hilos sean daemon
+    while True:
+        time.sleep(3600)
