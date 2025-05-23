aefc10f41e2b43271e7eecc29ca9a67f2cd43f22
pacomontorop <pacomontorop@gmail.com>
2025-05-23 12:38:09 +0200

    Update quiver_endpoints.py

--- signals/quiver_endpoints.py
+++ signals/quiver_endpoints.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 QUIVER_API_KEY = os.getenv("QUIVER_API_KEY")
-QUIVER_BASE_URL = "https://api.quiverquant.com/beta"
+QUIVER_BASE_URL = "https://api.quiverquant.com"
 HEADERS = {"x-api-key": QUIVER_API_KEY}
 
 def safe_quiver_request(url, retries=3, delay=2):
